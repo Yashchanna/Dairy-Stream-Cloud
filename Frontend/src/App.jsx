1d862389bf03d5f92b7c087061b8e857a5db11af
Ayan1024 <116819600+Ayan1024@users.noreply.github.com>
2026-01-22 18:31:07 +0530

    reformat app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,38 +1,57 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AddNewCustomerForm from './components/AddNewCustomerForm';
-import { Route, Routes } from 'react-router-dom';
-import AddNewAgentForm from './components/AddNewAgentForm';
-import DairyCustomerDashboard from './components/DairyCustomerDashboard';
-import AdminDashboard from './components/AdminDashboard';
-import CustomerRegister from './components/CustomerRegister';
-import CustomerDashboard from './components/CustomerDashboard';
-import AgentDashboard from './components/agentDashboard';
-import LoginPage from './pages/LoginPage';
-import RegisterNewuserPage from './pages/RegisterNewuserPage';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddNewCustomerForm from "./components/AddNewCustomerForm";
+import { Route, Routes } from "react-router-dom";
+import AddNewAgentForm from "./components/AddNewAgentForm";
+import DairyCustomerDashboard from "./components/DairyCustomerDashboard";
+import AdminDashboard from "./components/AdminDashboard";
+import CustomerDashboard from "./components/CustomerDashboard";
+import AgentDashboard from "./components/agentDashboard";
+import LoginPage from "./pages/LoginPage";
+import RegisterNewuserPage from "./pages/RegisterNewuserPage";
 function App() {
   return (
     <Routes>
       {/* need to change the route later */}
-        <Route path='/' element={<LoginPage></LoginPage>}></Route> 
-      <Route path='/register' element={<RegisterNewuserPage></RegisterNewuserPage>}></Route>
-    
-      <Route path='/customer-dashboard' element={<CustomerDashboard></CustomerDashboard>}></Route>
-      <Route path='admin/AdminDashboard' element={<AdminDashboard></AdminDashboard>}></Route>
-      <Route path='admin/addCustomer' element={<AddNewCustomerForm></AddNewCustomerForm>}></Route>
+      <Route path="/" element={<LoginPage></LoginPage>}></Route>
+      <Route
+        path="/register"
+        element={<RegisterNewuserPage></RegisterNewuserPage>}
+      ></Route>
 
-      <Route path='admin/addAgent' element={<AddNewAgentForm></AddNewAgentForm>}></Route>
-      <Route path='/customerDashbord' element={<DairyCustomerDashboard></DairyCustomerDashboard>}></Route>
-      <Route path='agent/AgentDashboard' element={<AgentDashboard></AgentDashboard>}></Route>
-      <Route path='*' element={<h2 className='text-center mt-5'>404 - Page Not Found</h2>}></Route>
+      <Route
+        path="/customer-dashboard"
+        element={<CustomerDashboard></CustomerDashboard>}
+      ></Route>
+      <Route
+        path="admin/AdminDashboard"
+        element={<AdminDashboard></AdminDashboard>}
+      ></Route>
+      <Route
+        path="admin/addCustomer"
+        element={<AddNewCustomerForm></AddNewCustomerForm>}
+      ></Route>
 
+      <Route
+        path="admin/addAgent"
+        element={<AddNewAgentForm></AddNewAgentForm>}
+      ></Route>
+      <Route
+        path="/customerDashbord"
+        element={<DairyCustomerDashboard></DairyCustomerDashboard>}
+      ></Route>
+      <Route
+        path="agent/AgentDashboard"
+        element={<AgentDashboard></AgentDashboard>}
+      ></Route>
+      <Route
+        path="*"
+        element={<h2 className="text-center mt-5">404 - Page Not Found</h2>}
+      ></Route>
     </Routes>
-   
-)
+  );
 }
-export default App
-
-
+export default App;
 
 ///this is uncommnet after we complete the whole app, so that we can use the protected route and give a smooth exp for the user also prevent clicking the wrong url
 
@@ -49,7 +68,7 @@ export default App
 // function App() {
 //   return (
 //     <Routes>
-      
+
 //       {/* Public: Login Page */}
 //       <Route path="/" element={
 //         <PublicRoute>
@@ -63,7 +82,7 @@ export default App
 //           <CustomerDashboard />
 //         </ProtectedRoute>
 //       } />
-      
+
 //       {/* ... other routes ... */}
 
 //     </Routes>
